49cb4633b0c95d815c90aba9daf280b3329b9dce
elias <eliasls2002@yahoo.de>
2023-09-20 11:21:34 +0200

    Created Player class

--- src/main/java/org/example/rummikub/setup/Hand.java
+++
@@ -1,4 +0,0 @@
-package org.example.rummikub.setup;
-
-public class Hand {
-}
---
+++ src/main/java/org/example/rummikub/setup/Player.java
@@ -0,0 +1,12 @@
+package org.example.rummikub.setup;
+
+import java.util.ArrayList;
+
+public class Player {
+    ArrayList<Tile> hand;
+
+    public Player (ArrayList<Tile> hand) {
+        this.hand = hand;
+    }
+    
+}
